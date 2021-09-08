9a4a4d068b13c18918c4e2c0d3fd7244c32b446d
mariamargarethsimarmata <90306485+mariamargarethsimarmata@users.noreply.github.com>
2021-09-08 16:07:22 +0700

    Add files via upload

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public KeyCode upButton = KeyCode.W; // Tombol untuk menggerakan ke atas
+    public KeyCode downButton = KeyCode.S; // Tombol untuk menggerakan ke bawah
+    public float speed = 10.0f; // kecepatan gerak
+    public float yBoundary = 9.0f; //Batas atas dan batas bawah game scene
+    private Rigidbody2D rigidBody2D; //Rigidbody2d raket
+    private int score; // Skor pemain
+    private ContactPoint2D lastContactPoint;
+    public ContactPoint2D LastContactPoint
+    {
+        get { return lastContactPoint; }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Equals("bola"))
+        {
+            lastContactPoint = collision.GetContact(0);
+        }
+    }
+
+    
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+       
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 velocity = rigidBody2D.velocity;// Dapatkan kecepatan raket sekarang
+        if (Input.GetKey(upButton))
+        {
+            velocity.y = speed;//jika menekan tombol w
+        }
+        else if (Input.GetKey(downButton))
+        {
+            velocity.y = -speed;
+        }
+        else
+        {
+            velocity.y = 0.00f;
+        }
+        rigidBody2D.velocity = velocity;// Masukkan kembali kecepatannya ke rigidBody2D.
+
+        Vector3 position = transform.position;//Mendapatkan posisi Raket
+        if (position.y > yBoundary)
+        {
+            position.y = yBoundary;// Jika posisi raket melewati batas atas kemuddian kembalikan ke batas atas
+        }
+        else if (position.y < -yBoundary)
+        {
+            position.y = -yBoundary;// Jika posisi raket melewati batas bawah kemuddian kembalikan ke batas bawah
+        }
+        transform.position = position;// Masukkan kembali posisi ke transform awal
+    }
+    public void IncrementScore()
+    {
+        score++; // Menaikkan skor sebanyak 1 poin
+    }
+
+    public void ResetScore()
+    {
+        score = 0;// mengembalikan skor menjadi 0
+    }
+
+    public int Score
+    {
+        get { return score; } // Mendapatkan nilai skor
+    }
+}
---
+++ Assets/Trajectory.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trajectory : MonoBehaviour
+{
+    public BallControl ballControl;
+    CircleCollider2D ballCollider;
+    Rigidbody2D ballRigidbody;
+
+    public GameObject ballAtCollision;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballRigidbody = ballControl.GetComponent<Rigidbody2D>();//dapatkan komponen rigidbody
+        ballCollider = ballControl.GetComponent<CircleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool drawBallAtCollision = false;
+        Vector2 offsetHitPoint = new Vector2();//titik tumbukan digeser, untuk menggambar ball at collision
+        RaycastHit2D[] circleCastHit2DArray = Physics2D.CircleCastAll(ballRigidbody.position, ballCollider.radius, ballRigidbody.velocity.normalized);
+
+        //untuk setiap titik tumbukan
+        foreach (RaycastHit2D circleCastHit2D in circleCastHit2DArray)
+        {
+            if (circleCastHit2D.collider != null &&
+                circleCastHit2D.collider.GetComponent<BallControl>() == null)
+            {
+                Vector2 hitPoint = circleCastHit2D.point;//tentukan titik tumbukan
+                Vector2 hitNormal = circleCastHit2D.normal;//tentukan normal di titik tumbukan
+                offsetHitPoint = hitPoint + hitNormal * ballCollider.radius;//tentukan offset hitpoint, yaitu titik tengah bola saat bertumbukan
+                DottedLine.DottedLine.Instance.DrawDottedLine(ballControl.transform.position, offsetHitPoint);
+
+                if (circleCastHit2D.collider.GetComponent<SideWall>() == null)//jika bukan sidewal, gambar pantulannya
+                {
+                    Vector2 inVector = (offsetHitPoint - ballControl.TrajectoryOrigin).normalized;//hitung vektor datang
+                    Vector2 outVector = Vector2.Reflect(inVector, hitNormal);//hitung vektor keluar
+                    float outDot = Vector2.Dot(outVector, hitNormal);//terjadi tumbukn tidak digambar
+                    if (outDot > -1.0f && outDot < 1.0)
+                    {
+                        DottedLine.DottedLine.Instance.DrawDottedLine(offsetHitPoint, offsetHitPoint + outVector * 10.0f);//gambar lintasan pantulan
+                        drawBallAtCollision = true;
+                    }
+                }
+                break;
+            }
+        }
+        if (drawBallAtCollision)
+        {
+            ballAtCollision.transform.position = offsetHitPoint;
+            ballAtCollision.SetActive(true);//gambar bola di prediksi
+        }
+        else
+        {
+            ballAtCollision.SetActive(false);//sembunyikan
+        }
+    }
+
+}
